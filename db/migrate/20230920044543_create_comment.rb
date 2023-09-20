44a660229b115cf38bb826f6575ecf0ea7ee4f1d
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-09-20 09:56:28 +0500

    fixing foreign keys in comment and post migration files

--- db/migrate/20230920044543_create_comment.rb
+++ db/migrate/20230920044543_create_comment.rb
@@ -2,8 +2,8 @@ class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comment do |t|
       t.text :body
-      t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :user_id, null: false, foreign_key: { to_table: :user }
+      t.references :post_id, null: false, foreign_key: { to_table: :post }
       t.datetime :updated_at
       t.datetime :created_at
       t.timestamps
--- db/migrate/20230920044923_create_post.rb
+++ db/migrate/20230920044923_create_post.rb
@@ -1,7 +1,7 @@
 class CreatePost < ActiveRecord::Migration[7.0]
   def change
     create_table :post do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :author_id, null: false, foreign_key: { to_table: :users }
       t.string :title
       t.text : text
       t.datetime :updated_at
