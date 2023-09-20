b91842e0ff12cc7622ed238fbfc28e3d079f36f5
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-09-20 09:52:41 +0500

    Changes in post migration file

--- db/migrate/20230920044923_create_post.rb
+++ db/migrate/20230920044923_create_post.rb
@@ -1,7 +1,13 @@
 class CreatePost < ActiveRecord::Migration[7.0]
   def change
-    create_table :posts do |t|
-
+    create_table :post do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title
+      t.text : text
+      t.datetime :updated_at
+      t.datetime :created_at
+      t.integer :comments_counter, default: 0
+      t.integer :Likes_counter, default: 0
       t.timestamps
     end
   end
