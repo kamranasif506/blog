714a060f8f80938bdbba39081d29163e9ee49ab9
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-09-21 15:45:35 +0500

    Method to Update Posts Counter for a User

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :comments
     has_many :likes
+
+    after_create :increment_user_posts_counter
+
+    private
+
+    def increment_user_posts_counter
+        author.increment!(:posts_counter)
+    end
 end
