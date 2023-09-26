77f657a6cdb970970e3cd9c3cd8f1fb9d99cbf89
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-09-26 18:07:44 +0500

    Created Posts controller and view files corresponding to it

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,8 @@
+class PostsController < ApplicationController
+    def index
+        render template: 'layouts/posts/index'
+    end
+    def show
+        render template: 'layouts/posts/show'
+    end
+end
