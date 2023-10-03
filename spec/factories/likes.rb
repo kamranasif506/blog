9df25a7830a25d00b4a6a4161d811e53d3a59d11
Ghulam Shabbir <adeeljatoi8899@gmail.com>
2023-10-03 14:05:57 +0500

    Revert "Integrated + n1"

--- spec/factories/likes.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :like do
-    association :author, factory: :user
-    association :post, factory: :post
-  end
-end
--- spec/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name { 'John Doe' }
-    photo { 'assets/images/default.jpg' }
-    bio { 'Mr: Kamran' }
-  end
-end
